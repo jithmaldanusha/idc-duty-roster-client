04fb361e70869123bf6f70be242481285fa2fbcc
danushajithmal <danushajithmal@gmail.com>
2025-03-05 13:39:19 +0530

    Dockerfile

--- my-app/src/app/components/taskBox/page.js
+++ my-app/src/app/components/taskBox/page.js
@@ -1,14 +1,13 @@
 import TaskCard from "../taskCard/page";
 
-export default function TaskBox({ taskList, onRemove }) {
+export default function TaskBox({ taskList = [], onRemove }) { // Set default to empty array
     return (
         <section>
             <div className="container border mt-3 mb-3">
                 <div className="row">
-                    {taskList.length > 0 ? (
+                    {Array.isArray(taskList) && taskList.length > 0 ? (
                         taskList.map((task, index) => (
-                            <div key={index} className="col-md-4 mb-3"> 
-                                {/* col-md-4 makes it 3 columns per row */}
+                            <div key={index} className="col-md-4 mb-3">
                                 <TaskCard task={task} onRemove={() => onRemove(index)} />
                             </div>
                         ))
