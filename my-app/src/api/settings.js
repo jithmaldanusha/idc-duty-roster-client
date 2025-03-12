908da687511b0c63a0f6ca870d873a35865362e6
danushajithmal <danushajithmal@gmail.com>
2025-03-12 10:11:05 +0530

    env added

--- my-app/src/api/settings.js
+++ my-app/src/api/settings.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000'; 
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL; 
 
 export const getRosterByDate = async (year, month) => {
     try {
